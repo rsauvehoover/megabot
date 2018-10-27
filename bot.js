7ff6d3e051fb2ec1fc83c38c10305ba22b19ae42
Jeff Cho <mail@jeffcho.com>
2018-10-26 20:32:00 -0600

    Completed roles/courses functionality and polished things up a little bit.

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json');
 
+var channelroles = ['artist', 'director', 'dev', 
+                    'musician', 'producer', 'writer']; 
+var courseroles = ['blaw-301', 'cmput-250', 'cmput-366', 'mlcs-399'];
+
 // Initialize Discord Bot
 var client = new Discord.Client();
 
@@ -34,90 +38,178 @@ function findChannel(guild, channelname) {
   return guild.channels.find(ch => ch.name === channelname);
 }
 
+//
 // Handle new joins
+//
 client.on('guildMemberAdd', member => {
 
   const ruleschan = findChannel(member.guild, 'rules');
+  const channel = findChannel(member.guild, 'welcome');
+  if (!channel) return;
 
   var newRole = findRole(member.guild, 'new');
   member.addRole(newRole)
-    .then(console.log)
+    .then(channel.send(`Hello and welcome to the Megachannel, ${member}! Please make sure you read the ${ruleschan.toString()} first - they're short, simple, and easy to follow. Once you have read and agreed to the rules, you will have access to all the regular channels on the server!`))
     .catch(console.error);
-
-  const channel = findChannel(member.guild, 'welcome');
-  if (!channel) return;
-  
-  channel.send(`Hello and welcome to the Megachannel, ${member}! Please make sure you read the ` + ruleschan.toString() + ` first - they\'re short, simple, and easy to follow. Once you have read and agreed to the rules, you will have access to all the regular channels on the server!`);
 });
 
+//
 // Respond to specific messages
+//
 client.on('message', msg => {
   
-  const generalchan = findChannel(msg.guild, 'general');
-  const reqchan = findChannel(msg.guild, 'requests');
-  const profileschan = findChannel(msg.guild, 'profiles');
-    
-  // New Member Agreement
-  if (msg.content == '!agree' && 
-        msg.channel == findChannel(msg.guild, 'welcome') &&
-        msg.member.roles.find("name", "new")) {
-      
-    msg.member.removeRole(findRole(msg.guild, 'new'))
-      .then(console.log)
-      .catch(console.error);
-      
-    msg.member.addRole(findRole(msg.guild, 'confirmed'))
-      .then(console.log)
-      .catch(console.error);
-        
-    msg.reply('You have agreed to the rules of this server! Please make sure you check back often to keep up-to-date with changes. \n\nYou can now use any publicly-available channel; for example, you don\'t have to be taking the course that corresponds to a course channel in order to chat there.  Feel free to head over to ' + profileschan.toString() + ' and introduce yourself - this is handy because the Megachannel has users who are in different programs and courses who might not know each other!  \n\nLastly, you may want to mute any channels you\'re not particularly interested in, as we can get into spirited discussions that can blow up your notifications.');
-      
-    generalchan.send(`Please welcome our newest member ${msg.member} to the Megachannel!`);
-  }
+  if (msg.content.substring(0,1) == '!') {
   
-  // Help Message
-  if (msg.content == '!help') {
-    msg.reply('pong');
-  }
+    var args = msg.content.substring(1).split(' ');
+    var cmd = args[0];
+    var sender = msg.member;
+    var isNew = sender.roles.find('name', 'new');
+    var isConfirmed = sender.roles.find('name', 'confirmed');
   
-  // Reset Permissions to Confirmed
-  else if (msg.content == '!reset' && 
-            msg.member.roles.find('name', 'confirmed')) {
+    const generalchan = findChannel(msg.guild, 'general');
+    const welcomechan = findChannel(msg.guild, 'welcome');
+    const reqchan = findChannel(msg.guild, 'requests');
+    const profileschan = findChannel(msg.guild, 'profiles');
     
-    for (var [id, role] of msg.guild.roles) {
-      if (role.name != '@everyone' && role.name != 'confirmed') {
-        console.log('Remove!');
-        msg.member.removeRole(role)
+    // New Member Agreement - on welcome channel, role new
+    if (cmd == 'agree') {
+      
+      if (msg.channel == welcomechan && isNew) {
+        sender.removeRole(findRole(msg.guild, 'new'))
+          .then(console.log)
           .catch(console.error);
+      
+        sender.addRole(findRole(msg.guild, 'confirmed'))
+          .then(console.log)
+          .catch(console.error);
+        
+        sender.send('You have agreed to the rules of this server! Please make sure you check back often to keep up-to-date with changes. \n\nYou can now use any publicly-available channel; for example, you don\'t have to be taking the course that corresponds to a course channel in order to chat there.  Feel free to head over to the ' + profileschan.toString() + ' channel and introduce yourself - this is handy because the Megachannel has users who are in different programs and courses who might not know each other!  \n\nLastly, you may want to mute any channels you\'re not particularly interested in, as we can get into spirited discussions that can blow up your notifications.');
+      
+        generalchan.send(`Please welcome our newest member ${msg.member} to the Megachannel!`);
+      
+      } else {
+        sender.send('You have already agreed to the rules on this server.')
       }
     }
-    
-    msg.reply('Your permissions to the server have been reset.');
-
-  }
   
-  // Get Profile - only for confirmed members
-  else if (msg.content.substring(0, 8) == '!profile'
-            && msg.member.roles.find('name', 'confirmed')) {
+    // Help Message
+    if (cmd == 'help') {
     
-    var user = msg.mentions.members.first();
-    var profile = profileschan.fetchMessages()
-                  .then(messages => 
-                      messages.filter(m => m.author.id === user.id))
-                  .then(message => {
+      var helpheader = 'You can use the following commands (replace anything in <angle brackets> with an argument - e.g. type \`!profile @MegaBot\`, not \`!profile <@MegaBot>\\`):\n\n' +
+                        '\`!help\`: show this help message\n';
+      
+      var helptext = '';
+    
+      if (isNew) {
+      
+        helptext = '\`!agree\`: Agree to the rules of the server.';
+      
+      } else {
+        
+        helptext = '\`!profile <@user>\`: find link to user\'s profile\n' +
+                  '\`!role <role>\`: set yourself as <role> (one per command) so you can be mentioned using @<role>. You can have as many <role>s as you want. If you enter a <role> that you already have, it will be removed.\n' +
+                  '\tRoles: artist, director, dev, musician, producer, writer\n' +
+                  '\`!course <course>\`: set yourself as being in <course> (one per command) so you can be mentioned using @<course>. You can have as many <course>s as you want. If you enter a <course> that you already have, it will be removed.\n' + 
+                  '\tCourses: any currently listed in the Courses channel group\n';
+      }
+      
+      msg.author.send(helpheader + helptext);
+    }
+  
+    // Reset Permissions to Confirmed
+    else if (cmd == 'reset' && isConfirmed) {
+    
+      for (var [id, role] of msg.guild.roles) {
+        if (role.name != '@everyone' && role.name != 'confirmed') {
+          console.log('Remove!');
+          msg.member.removeRole(role)
+            .catch(console.error);
+        }
+      }
+    
+      msg.author.send('Your permissions to the server have been reset. Please add back any roles you want on your profile.');
+
+    }
+    
+    // Toggle role
+    else if (cmd == 'role' || cmd == 'course') {
+    
+      if (isConfirmed) {
+        if (args.length == 2) {
+        
+          role = args[1];
+          
+          if ((cmd == 'role' && !channelroles.includes(role)) || 
+              (cmd == 'course' && !courseroles.includes(role))) {
+            sender.send(`The ${cmd} \`${role}\` does not exist or cannot be added using this command.`);
+            return;
+          }
+          
+          if (!findRole(msg.guild, role)) {
+            sender.send(`The ${cmd} \`${role}\` could not be found on this server. Please try again.`);
+            return;
+          }
+          
+          // Has role - remove it
+          if (sender.roles.find('name', role)) {
+          
+            sender.removeRole(findRole(msg.guild, role))
+              .then(sender.send(`The ${cmd} '${role}' was removed. You will no longer be notified when \`@${role}\` is mentioned.`))
+              .catch(console.error);
+          
+          // Doesn't have role - add it
+          } else {
+          
+            sender.addRole(findRole(msg.guild, role))
+              .then(sender.send(`The ${cmd} '${role}' was added. You will now be notified when someone mentions \`@${role}\`.`))
+              .catch(console.error);
+          }
+          
+          
+        } else {
+        
+          if (cmd == 'role') {
+            sender.send("The command must be of the format: \`!role <rolename>\` where <rolename> can be artist, director, dev, musician, producer, or writer.");
+          } else if (cmd == 'course') {
+            sender.send("The command must be of the format: \`!course <coursename>\` where <coursename> must match one of the course channel names.");
+          }
+        }
+      }
+    }
+  
+    // Get Profile
+    else if (cmd == 'profile') {
+    
+      // Only give profiles to confirmed members
+      if (isConfirmed) {
+    
+        var user = msg.mentions.members.first();
+        var profile = profileschan.fetchMessages()
+                      .then(messages => 
+                          messages.filter(m => m.author.id === user.id))
+                      .then(message => {
                     
-                    if (message.size == 0) {
-                      msg.reply(`${user.toString()} has not yet posted to ${profileschan.toString()}.`);
-                    }
+                        if (message.size == 0) {
+                          msg.author.send(`${user.toString()} has not yet posted to ${profileschan.toString()}.`);
+                        }
                     
-                    for (var [id, m] of message) {
-                      msg.reply(`You can find ${user.toString()}'s profile at: ${m.url}.`);
-                    }
-                  })
-                  .catch(console.error);
+                        for (var [id, m] of message) {
+                          msg.author.send(`You can find ${user.toString()}'s profile at: ${m.url}.`);
+                        }
+                      })
+                      .catch(console.error);
+                    
+      // Non-confirmed members get an error message for now.                    
+      } else {
+        msg.author.send("You must agree to the rules to view any profiles.")
+      }    
+    }
     
-  }
-    
+    // Started with ! but didn't match any of the above
+    else {
+      msg.author.send('Your command \`' + msg.content + '\` was not recognized. Please check it and try again, or type \`!help\` for options.')
+    }
+  } 
 });
 
 // Client login
